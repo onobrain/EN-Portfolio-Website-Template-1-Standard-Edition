6189adf7a04e69fc57e3261a8d8304b5c97e1b79
hamza <dreemerups@gmail.com>
2022-09-09 10:44:15 +0200

    added preload animation

--- javascript/logic.js
+++ javascript/logic.js
@@ -60,3 +60,10 @@ document.querySelector("#hero .container .sidebar .burger").addEventListener("cl
         document.querySelector("#hero .container .navbar").classList.remove("open-nav")
     })
 })
+// preload
+const preloader =  document.querySelector('.preload_container');
+window.addEventListener('load',vanish)
+
+function vanish(){
+    preloader.classList.add('disappear');
+}
